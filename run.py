9ab27d514dbcc3c022aac258f0b5a216e41f6692
luong mon <ducmonmz@gmail.com>
2024-07-09 10:12:15 +0700

    Update Auth and UserSettings

--- run.py
+++ run.py
@@ -1,12 +1,13 @@
+from datetime import timedelta
 import time
-from flask import Flask, request, jsonify, current_app
+from flask import Flask, abort, json, request, jsonify, current_app
 from flask_cors import CORS
 from dotenv import load_dotenv
 import os
 
 from openai import OpenAI
 from config import Config
-from models.auth import auth_login, auth_register
+from models.auth import auth_login, auth_refresh_token, auth_register
 from models.chat import get_chat_completions
 from models.models import get_models
 from flask_bcrypt import Bcrypt
@@ -20,11 +21,14 @@ load_dotenv()
 app = Flask(__name__)
 app.config.from_object(Config)
 bcrypt = Bcrypt(app)
+app.config['JWT_ACCESS_TOKEN_EXPIRES'] = timedelta(minutes=15)  # Access token expiration time
+app.config['JWT_REFRESH_TOKEN_EXPIRES'] = timedelta(days=30)  # Refresh token expiration time
 jwt = JWTManager(app)
 
 users = []  # Temporary in-memory user store
 
-CORS(app)  # Enable CORS for all domains
+# Configure CORS to allow all domains for all routes and methods
+CORS(app, resources={r"/api/*": {"origins": "*", "methods": ["GET", "PUT", "POST", "DELETE", "OPTIONS"], "allow_headers": ["Authorization", "Content-Type"]}})
 
 # Secret key for session management
 # app.secret_key = os.getenv('SECRET_KEY')
@@ -32,6 +36,20 @@ CORS(app)  # Enable CORS for all domains
 app.openAIClient = OpenAI(
     api_key=app.config['OPENAI_API_KEY'],
 )
+
+@app.after_request
+def after_request_func(response):
+    if response.is_json:
+        data = response.get_json()
+        msg = data.get('msg')
+        if msg:
+            data['message'] = msg
+            response.set_data(json.dumps(data))
+    # response.headers.add('Access-Control-Allow-Origin', '*')
+    # response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
+    # response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
+    return response
+
 
 @app.route('/', methods=['GET', 'PUT', 'POST', 'OPTIONS'])
 def index():
@@ -90,6 +108,19 @@ def register():
     else:
         return jsonify(auth_data), 401
 
+# /api/v1/auth/refreshtoken
+@app.route('/api/v1/auth/refreshtoken', methods=['POST', 'OPTIONS'])
+@app.route('/api/v1/auth/refreshtoken/', methods=['POST', 'OPTIONS'])
+@jwt_required(refresh=True)
+def refreshtoken():
+    if request.method == 'OPTIONS':
+        return '', 200
+    data = auth_refresh_token()
+    if data['status'] == 'success':
+        return jsonify(data)
+    else:
+        return jsonify(data), 401
+
 
 # /api/v1/chat
 @app.route('/api/v1/chat', methods=['GET', 'PUT', 'POST', 'OPTIONS'])
@@ -113,7 +144,11 @@ def chat_completions_v1():
 def models_v1():
     if request.method == 'OPTIONS':
         return '', 200
-    return jsonify(get_models(request))
+    data = get_models()
+    if data['status'] == 'success':
+        return jsonify(data)
+    else:
+        return jsonify(data), 401
 
 # /api/v1/settings/google
 @app.route('/api/v1/settings/google', methods=['GET', 'PUT', 'POST', 'OPTIONS'])
@@ -126,11 +161,20 @@ def settings_google_v1():
         return jsonify(data)
     else:
         return jsonify(data), 401
-    
-# /api/v1/settings
-@app.route('/api/v1/settings', methods=['GET', 'PUT', 'POST', 'OPTIONS'])
-@app.route('/api/v1/settings/', methods=['GET', 'PUT', 'POST', 'OPTIONS'])
-def settings_v1():
+
+# /api/v1/user
+@app.route('/api/v1/user', methods=['GET', 'PUT', 'POST', 'OPTIONS'])
+@app.route('/api/v1/user/', methods=['GET', 'PUT', 'POST', 'OPTIONS'])
+def user_v1():
+    if request.method == 'OPTIONS':
+        return '', 200
+    return jsonify({'message': 'Welcome to the User API v1!'})
+
+# /api/v1/user/settings
+@app.route('/api/v1/user/settings', methods=['GET', 'PUT', 'OPTIONS'])
+@app.route('/api/v1/user/settings/', methods=['GET', 'PUT', 'OPTIONS'])
+@jwt_required()
+def user_settings_v1():
     if request.method == 'OPTIONS':
         return '', 200
     switcher = {
