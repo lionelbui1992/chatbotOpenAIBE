5f889fce6266b77ef99169c4cecead93b7ce012a
luong mon <ducmonmz@gmail.com>
2024-07-09 22:50:37 +0700

    Update google settings

--- run.py
+++ run.py
@@ -15,7 +15,7 @@ from flask_bcrypt import Bcrypt
 from flask_jwt_extended import JWTManager, create_access_token, jwt_required, get_jwt_identity
 
 from models.google import user_google_connect
-from models.user_settings import get_user_settings, set_user_settings
+from models.user_settings import get_user_settings, set_user_setting_google, set_user_settings
 
 load_dotenv()
 
@@ -151,18 +151,6 @@ def models_v1():
     else:
         return jsonify(data), 401
 
-# /api/v1/settings/google
-@app.route('/api/v1/settings/google', methods=['GET', 'PUT', 'POST', 'OPTIONS'])
-@app.route('/api/v1/settings/google/', methods=['GET', 'PUT', 'POST', 'OPTIONS'])
-def settings_google_v1():
-    if request.method == 'OPTIONS':
-        return '', 200
-    data = user_google_connect(request)
-    if data['status'] == 'success':
-        return jsonify(data)
-    else:
-        return jsonify(data), 401
-
 # /api/v1/user
 @app.route('/api/v1/user', methods=['GET', 'PUT', 'POST', 'OPTIONS'])
 @app.route('/api/v1/user/', methods=['GET', 'PUT', 'POST', 'OPTIONS'])
@@ -183,6 +171,18 @@ def user_settings_v1():
         'PUT': set_user_settings
     }
     data = switcher[request.method](request)
+    if data['status'] == 'success':
+        return jsonify(data)
+    else:
+        return jsonify(data), 401
+    
+# /api/v1/user/google
+@app.route('/api/v1/user/google', methods=['PUT', 'OPTIONS'])
+@app.route('/api/v1/user/google/', methods=['PUT', 'OPTIONS'])
+def user_settings_google_v1():
+    if request.method == 'OPTIONS':
+        return '', 200
+    data = set_user_setting_google(request)
     if data['status'] == 'success':
         return jsonify(data)
     else:
