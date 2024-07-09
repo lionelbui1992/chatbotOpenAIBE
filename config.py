cd027a6f921525f098cd6695e16510368d808ad6
luong mon <ducmonmz@gmail.com>
2024-07-09 13:16:52 +0700

    Add key

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ from dotenv import load_dotenv
 class Config:
     # Get config from .env file
     load_dotenv()
-    SECRET_KEY = os.environ.get('JWT_SECRET_KEY', 'you-will-never-guess')
+    JWT_SECRET_KEY = os.environ.get('JWT_SECRET_KEY', 'you-will-never-guess')
     DATABASE_TYPE = 'mongodb'
     DB_NAME = os.environ.get('DB_NAME', 'sample_mflix')
     MONGO_URI = os.environ.get('MONGO_URI', 'mongodb+srv://lionelbui:pEciuiTKR28LKOMs@cluster0.hm7buca.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
@@ -16,3 +16,8 @@ class Config:
     OPENAI_API_KEY = os.environ.get('OPENAI_API_KEY')
     OPENAI_MODEL = os.environ.get('OPENAI_MODEL', 'gpt-3.5-turbo')
     EMBEDDING_MODEL = os.environ.get('EMBEDDING_MODEL', 'text-embedding-3-small')
+    GOOGLE_CLIENT_ID = os.environ.get('GOOGLE_CLIENT_ID')
+    GOOGLE_CLIENT_SECRET = os.environ.get('GOOGLE_CLIENT_SECRET')
+    GOOGLE_REDIRECT_URI = os.environ.get('GOOGLE_REDIRECT_URI')
+    GOOGLE_AUTHORIZATION_URL = os.environ.get('GOOGLE_AUTHORIZATION_URL')
+    GOOGLE_TOKEN_URL = os.environ.get('GOOGLE_TOKEN_URL')
