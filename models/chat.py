ef7a00fa7f779b26302fb62aceff825dcfd39db2
hyperspacehq <andrewt@hyperspacehq.com>
2024-07-10 16:16:16 +0700

    update prompt

--- models/chat.py
+++ models/chat.py
@@ -311,8 +311,8 @@ def get_chat_completions(request):
         target_score       = 0 # target score to show message
 
         # prompt for message in aggregate_result, should be manage by tags
-        messages.append({"role": "system", "content": "Hey OAS Asisstant! please display the best info bellow:" })
-        messages.append({"role": "user", "content":input_text})
+       # messages.append({"role": "system", "content": "Hey OAS Asisstant! please display the best info bellow:" })
+        messages.append({"role": "system", "content":input_text})
         
         # messages.append({"role": "user", "content": "Total is "+ str(total_row) + " Developers"})
         #print(messages)
